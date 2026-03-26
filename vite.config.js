466beb34e5fc84e5de8ab95b1ddc0907f638fb78
rajakannathasanmaghil <rajakannathasan@gmail.com>
2026-03-26 21:51:49 +0530

    fix deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/dist/',   // 👈 ADD THIS LINE
+  base: '/',   // 👈 ADD THIS LINE
   plugins: [react()],
 })
